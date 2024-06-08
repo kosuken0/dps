72ec316d0c4f9b2b4f9f1d34e81360c46fa292e1
KosukenSan <119076494+KosukenSan@users.noreply.github.com>
2024-06-08 19:18:27 +0100

    Update script.js

--- script.js
+++ script.js
@@ -20,41 +20,68 @@ document.getElementById("calculateButton").addEventListener("click", function ()
         const theta = 2 * Math.acos(((r1 ** 2 + d ** 2 - r2 ** 2) / (2 * r1 * d))) * (Math.PI / 180);
         const eta = Math.ceil(ph / (md - pa));
 
+        console.log("theta:", theta);
+        console.log("eta:", eta);
+
         let delta_norm, delta_poison, delta_lightn;
 
         switch (caseType) {
             case 'normal':
                 {
                     const numr = (pd - ma + bd) * am * eta;
+                    console.log("numr:", numr);
                     const den1 = (eta * 0.04) / (theta / rs);
+                    console.log("den1:", den1);
                     const den2 = Math.floor(den1 * ((2 * Math.PI - theta) / rs));
+                    console.log("den2:", den2);
                     const den3 = den2 + (eta * 0.04);
+                    console.log("den3:", den3);
                     const den4 = den3 + pr;
+                    console.log("den4:", den4);
                     const nden = 2 * Math.PI - (rs / (1 / pr));
+                    console.log("nden:", nden);
                     const denom = den4 + (nden / rs);
+                    console.log("denom:", denom);
                     delta_norm = numr / denom;
+                    console.log("delta_norm:", delta_norm);
+
                     break;
                 }
             case 'poison':
                 {
                     const numr = (pd - ma + bd) * am * eta;
+                    console.log("numr:", numr);
                     const den1 = (eta * 0.04) / (theta / rs);
+                    console.log("den1:", den1);
                     const den2 = Math.floor(den1 * ((2 * Math.PI - theta) / rs));
+                    console.log("den2:", den2);
                     const den3 = den2 + (eta * 0.04);
+                    console.log("den3:", den3);
                     const den4 = den3 + pr;
+                    console.log("den4:", den4);
                     const nden = 2 * Math.PI - (rs / (1 / pr));
+                    console.log("nden:", nden);
                     const denom = den4 + (nden / rs);
+                    console.log("denom:", denom);
                     delta_norm = numr / denom;
+                    console.log("delta_norm:", delta_norm);
 
                     const pden = (po / tp) + Math.max(0, ((2 * Math.PI - theta) / rs) - rs);
+                    console.log("pden:", pden);
                     delta_poison = tp / pden;
+                    console.log("delta_poison:", delta_poison);
+
                     break;
                 }
             case 'lightning':
                 {
                     const le1 = (2 * Math.PI - (rs / (1 / pr))) / rs;
+                    console.log("le1:", le1);
                     const le2 = pr + 0.04 + le1;
+                    console.log("le2:", le2);
                     delta_lightn = (ld * am) / le2;
+                    console.log("delta_lightn:", delta_lightn);
+
                     break;
                 }
             default:
